667e4898ce360faa26f0d29970fddeb46eac2d69
asaran97 <asaran234@gmail.com>
2019-12-02 22:49:38 +0530

    section 3

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-const express = require('express');
+const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require('morgan')
 
-dotenv.config({ path: './config/config.env' })
+//rourtes
+const bootcamps = require('./routes/bootcamps')
+
+//load env vars
+dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+//logging middleware
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use('/api/v1/bootcamps',bootcamps)
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log( `server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
