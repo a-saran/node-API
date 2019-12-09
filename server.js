d5898041c9c5a8dee12f044a40f607bd5ae21f2a
asaran97 <asaran234@gmail.com>
2019-12-09 22:08:31 +0530

    basic CRUD opertaion in bootcamps

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require('morgan')
+const morgan = require('morgan');
 
-//rourtes
-const bootcamps = require('./routes/bootcamps')
+const connectDB = require('./config/db');
 
 //load env vars
 dotenv.config({ path: "./config/config.env" });
 
+//connect to DB
+connectDB();
+
+//routes
+const bootcamps = require('./routes/bootcamps')
+
 const app = express();
+
+//body-parser
+app.use(express.json());
 
 //logging middleware
 if(process.env.NODE_ENV === 'development') {
@@ -19,6 +27,14 @@ app.use('/api/v1/bootcamps',bootcamps)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
+const server = app.listen(PORT, () =>
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//handle unhandled promise rejection
+process.on('unHandledRejection', (err, promise) => {
+  console.log(`Error : ${err}`)
+
+  //close server
+  server.close(() => process.exit(1));
+})
