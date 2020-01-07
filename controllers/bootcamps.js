6ceb2ac497e686c103170102de826dbf8126e158
asaran97 <asaran234@gmail.com>
2020-01-07 22:58:48 +0530

    added sorting and select feature

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -10,10 +10,36 @@ const asyncHandler = require("./../middleware/async");
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
   let query;
 
-  let queryStr = JSON.stringify(req.query);
+  //copy od req.query
+  const reqQuery = { ...req.query}
+
+  //Fields exclude
+  const removeFields =['select', 'sort'];
+
+  //loop over removeFields and delete from reqQuery
+  removeFields.forEach(param => delete reqQuery[param])
+
+  //query string
+  let queryStr = JSON.stringify(reqQuery);
+
   queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
 
+  //finding resource
   query = Bootcamp.find(JSON.parse(queryStr));
+
+  //Select Fields
+  if(req.query.select) {
+    const fields = req.query.select.split(',').join(' ');
+    query = query.select(fields)
+  }
+
+  //sort
+  if(req.query.sort) {
+    const sortBy = req.query.sort.split(',').join(' ');
+    query = query.sort(sortBy)
+  } else {
+    query = query.sort('-createdAt')
+  }
 
   const bootcamps = await query;
 
